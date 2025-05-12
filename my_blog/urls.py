7deb27870cef384d5b6260ee53bce806866781ff
Betul Mumcu <betuullm01@gmail.com>
2025-05-12 18:31:26 +0300

    changes

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'blog/static'))
